2c08e4821f5931e2e5a75b46b025204a717a1516
KyleCSharp <kylelrigler@gmail.com>
2022-08-24 19:09:28 -0400

    part 2 of 1

---
+++ OperatorsEx2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperatorsEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radis of your circle?");
+                var radius = double.Parse(Console.ReadLine());
+            var AreaCircle = AreaOfCircle (radius);
+            Console.WriteLine($"The area of your circle of {radius} is {AreaCircle}");
+
+
+        }
+        public static double AreaOfCircle (double radius)
+        {
+
+            return Math.PI * ( radius * radius);
+        }
+    }
+}
